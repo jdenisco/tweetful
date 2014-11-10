09d74c8acef5615b264ef89ec4fc59b338932aef
James Denisco <james.denisco@genesys.com>
2014-11-10 14:02:42 -0500

    Added some functionality. Able to get friends ids, can convert ids into names. Also I get get trending info based on WOEID

--- tweetful.py
+++ tweetful.py
@@ -21,14 +21,63 @@ import requests
 
 from urls import *
 
+
+def friend_ids():
+    """ friends function """
+    try:
+        auth = authorization.authorize()
+        r = requests.get(FRIENDS_LIST, auth=auth)
+        flist = json.loads(r.content)
+        for ids in flist['ids']:
+            print ids
+
+    except (requests.exceptions.RequestException,Exception) as e:
+      print e 
+    
+def friend_id_2_name(id):
+    """ Converts friends id into readable name """
+    try:
+        auth = authorization.authorize()
+        rid = requests.get(FRIENDS_ID2NAME + '&user_id=' + id, auth=auth)
+        rid_name = json.loads(rid.content)
+        for name in rid_name:
+            return name['name']
+
+    except (requests.exceptions.RequestException,Exception) as e:
+      print e 
+
+def trendingplaces(WOEID=1):
+    """ Trending places """
+    a = 0
+    try:
+        auth = authorization.authorize()
+        rtrend = requests.get(TRENDS_PLACE + str(WOEID), auth=auth)
+        rtrend_return = json.loads(rtrend.content)
+        for place in rtrend_return:
+            while True: 
+                if place['trends'][a]['name'] > 0 :
+                    topic = place['trends'][a]['name']
+                    urls = place['trends'][a]['url']
+                    print type(urls)
+                    print("Trending Topics %s for more info %s " % (topic, str(urls)))
+                    a += 1
+                else:
+                   print "done"
+
+    except (requests.exceptions.RequestException,Exception) as e:
+      print e 
+
 def main():
     """ Main function """
-    auth = authorization.authorize()
+#    auth = authorization.authorize()
 
-    response = requests.get(TIMELINE_URL, auth=auth)
-    print json.dumps(response.json(), indent=4)
+#    response = requests.get(TIMELINE_URL, auth=auth)
+#    print json.dumps(response.json(), indent=4)
+
+    friend_ids()
+    print(friend_id_2_name('18936345'))
+    trendingplaces()
+
 
 if __name__ == "__main__":
     main()
-
-
--- urls.py
+++ urls.py
@@ -19,5 +19,6 @@ REQUEST_TOKEN_URL = API_URL + "/oauth/request_token"
 AUTHORIZE_URL = API_URL + "/oauth/authorize?oauth_token={request_token}"
 ACCESS_TOKEN_URL = API_URL + "/oauth/access_token"
 TIMELINE_URL = API_URL + "/1.1/statuses/home_timeline.json"
-
-
+FRIENDS_LIST= API_URL + "/1.1/friends/ids.json?cursor=-1&screen_name=jimdenisco&count=5000"
+FRIENDS_ID2NAME = API_URL + "/1.1/users/lookup.json?screen_name=jimdenisco,twitter"
+TRENDS_PLACE = API_URL + '/1.1/trends/place.json?id='
